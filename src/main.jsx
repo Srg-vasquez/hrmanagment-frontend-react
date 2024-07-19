10858b56a72140641482fb039528e15a6e24f1d7
Sergio-Vasquez <sergio.vasquez27@inacapmail.cl>
2024-07-19 01:00:54 -0400

    add(buscarFuncionario)
    se agrega gestionar funcionario

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/index.css';
 import BuscarFuncionario from './BuscarFuncionario.jsx';
-import GesionarFuncionario from './GestionarFuncionario.jsx';
+import GestionarFuncionario from './GestionarFuncionario.jsx';
 import InformacionPersonal from './InformacionPersonal.jsx';
 import IngresarTrabajador from './IngresarTrabajador.jsx';
 import ListarTrabajador from './ListarTrabajador.jsx';
 import LogIn from './Log-In.jsx';
 import UsersPage from './users.jsx';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<LogIn />} />
         <Route path="/buscar_funcionario" element={<BuscarFuncionario />} />
-        <Route path="/gestionar_funcionario" element={<GesionarFuncionario />} />
+        <Route path="/gestionar_funcionario" element={<GestionarFuncionario />} />
         <Route path="/informacion_personal" element={<InformacionPersonal />} />
         <Route path="/ingresar_trabajador" element={<IngresarTrabajador />} />
         <Route path="/listar_trabajador" element={<ListarTrabajador />} />
